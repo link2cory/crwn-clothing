30de387a740103587b386f5c2fe4128cfddfe0f6
Cory Perkins <link2cory@gmail.com>
2020-07-15 10:04:26 -0700

    fix typo in filename: cutom-button.component.jsx => custom-button.component.jsx

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/cutom-button.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
