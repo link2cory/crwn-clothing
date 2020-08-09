44f579ed8304749173163b14491dd4f79222e470
Cory Perkins <link2cory@gmail.com>
2020-08-09 14:34:24 -0700

    setup user authentication persistence using redux-saga

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import { selectCurrentUser } from "./redux/user/user.selector";
+import { checkUserSession } from "./redux/user/user.actions";
 
 import "./App.css";
 
@@ -16,20 +17,8 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-    // if (userAuth) {
-    // const userRef = await createUserProfileDocument(userAuth);
-    //
-    // userRef.onSnapshot((snapshot) => {
-    // setCurrentUser({
-    // id: snapshot.id,
-    // ...snapshot.data(),
-    // });
-    // });
-    // } else {
-    // setCurrentUser(userAuth);
-    // }
-    // });
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -65,4 +54,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
